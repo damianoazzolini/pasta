ad9d1463554248b41a1b37cbe2c607ccbe64bc67
Damiano Azzolini <damiano.azzolini@unife.it>
2022-01-24 17:18:59 +0100

    to prolog converter

--- src/pasta/generator.py
+++ src/pasta/generator.py
@@ -1,5 +1,8 @@
-from typing import Counter, Union
+from typing import Union
 import sys
+
+import utils
+
 '''
 Class defining a generator of an ASP program
 '''
@@ -13,7 +16,7 @@ class Generator:
     # dom_f(1..3). from f(1..3).
     # dom_a from a
     @staticmethod
-    def generate_clauses_from_facts(term: str, probability: float, precision: int):
+    def generate_clauses_for_facts(term: str, probability: float, precision: int):
         generator_term = '0{' + term + '}1.'
 
         if "(" not in term:
@@ -41,7 +44,7 @@ class Generator:
         return [var for var in term if (len(var) > 0 and var[0].isupper())]
 
     @staticmethod
-    def expand_conditional(conditional : str) -> list:
+    def generate_clauses_for_conditionals(conditional : str) -> list:
         if "|" not in conditional:
             sys.exit("Syntax error in conditional: " + conditional)
         if "[" not in conditional or "]" not in conditional:
@@ -98,13 +101,126 @@ class Generator:
         return [disjunct,cu,cl]
 
     @staticmethod
-    def expand_abducible(line: str) -> Union[str,str]:
+    def generate_clauses_for_abducibles(line: str) -> Union[str,str]:
         if len(line.split(' ')) != 2:
-            sys.exit("Error in line " + line)
+            utils.print_error_and_exit("Error in line " + line)
         
         if not line.endswith('.'):
-            sys.exit("Missing final . in " + line)
+            utils.print_error_and_exit("Missing final . in " + line)
         
         # TODO: add sanity checks for the atom: no variables and correct syntax
         line = line.split(' ')
-        return '0{' + line[1][:-1] + '}1.', line[1][:-1]
+        term = line[1][:-1]
+        generator = '0{' + term + '}1.'
+        t1 = "abd_" + term
+
+        if "(" not in term:
+            commas = 0
+        else:
+            commas = term.count(',') + 1
+
+        new_fact_true = t1 + ':- ' + term + '.'
+
+        new_fact_false = "not_" + t1 + ' :- not ' + term + '.'
+
+        show_true = "#show " + t1 + "/" + str(commas) + "."
+
+        show_false = "#show not_" + t1 + "/" + str(commas) + "."
+
+        return [generator, new_fact_true, new_fact_false, show_true, show_false], term
+
+    '''
+    Creates a prolog version of the line to identify the minimal set
+    of probabilistic and abducibles fact, kind of hack
+    '''
+    @staticmethod
+    def to_prolog(line : str) -> str:
+        res = []
+        line = line.split(':-')
+        # process body
+        if len(line) == 2:
+            body = list(line[1])
+
+            i = 0
+            while i < len(body):
+                if body[i] == '}':
+                    while i < len(body) and body[i] != ',' and body[i] != '.':
+                        body[i] = ' '
+                        i = i + 1
+                elif body[i] == '#':
+                    # extract everything until }
+                    while body[i] != ':':
+                        body[i] = ' '
+                        i = i + 1
+                    body[i] = ' '
+                    i = i + 1
+                    temp = []
+                    while i < len(body) and body[i] != '}':
+                        temp.append(body[i])
+                        body[i] = ' '
+                        i = i + 1
+                    body[i] = ' '  # }
+                    # insert in the current position
+                    body[i:i] = ['newGoalInserted', str(goal_index), ',']
+                    i = i + 3
+
+                    body_new = ''.join(temp).replace(' ', '')
+                    # insert the new goal
+                    goal = "newGoalInserted" + str(goal_index) + ':-' + body_new + '.'
+                    goal_index = goal_index + 1
+                    res.append(goal)
+                elif body[i] == '=' or body[i] == '<' or body[i] == '>':
+                    while i < len(body) and body[i] != ',' and body[i] != '.':
+                        body[i] = ' '
+                        i = i + 1
+                    body[i] = ' '
+                    i = i + 1
+                else:
+                    i = i + 1
+        else:
+            body = None
+
+        if body is not None:
+            body = ''.join(body).replace(' ', '')
+            # case where the goal is the last to be inserted
+            if body.endswith(','):
+                body = body[:-1] + '.'
+
+        # process head
+        head = line[0]
+        if ';' in head:
+            # disjunctive head
+            head = head.split(';')
+            for h in head:
+                res.append(h + (':-' + body if body is not None else ''))
+        elif '{' in head:
+            #head aggregate
+            head = list(head)
+            i = 0
+            while i < len(head) and head[i] != '{':
+                head[i] = ' '
+                i = i + 1
+            head[i] = ' '
+            i = i + 1
+
+            while i < len(head) and head[i] != '}':
+                i = i + 1
+            head[i] = ' '
+            i = i + 1
+
+            while i < len(head) and head[i] != ':' and head[i] != '.':  # . or :- terminator
+                head[i] = ' '
+                i = i + 1
+
+            head = ''.join(head).replace(' ', '')
+            res.append(head + (':-' + body if body is not None else ''))
+        elif head == '':
+            # integrity constraint
+            head = "newGoalInserted" + str(goal_index)
+            res.append(head + (':-' + body if body is not None else ''))
+
+        else:
+            res.append(head + (':-' + body if body is not None else ''))
+    
+        return res
+
