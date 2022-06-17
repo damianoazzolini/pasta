a347e164e1d022c5a2a17884d183e7e15284892c
Damiano Azzolini <damiano.azzolini@unife.it>
2022-06-17 10:39:32 +0200

    new pipelines

---
+++ scripts/lint_test_pipeline.sh
@@ -0,0 +1,9 @@
+echo "--- lint with pylint ---"
+echo "disabled W0311: bad indentation"
+python3 -m pylint ../src/pasta --disable=W0311
+
+echo "--- lint with flake 8 ---"
+flake8 ../ --show-source --statistics
+
+echo "-- test with pytest ---"
+cd .. && cd tests && pytest
