f5bb3153cab7b90304ad70a7e1555b228980649b
Damiano Azzolini <damiano.azzolini@unife.it>
2022-07-07 15:21:00 +0200

    fixed generators and examples for conditionals

--- benchmark/map/generate_gold_map_maximize.py
+++ benchmark/map/generate_gold_map_maximize.py
@@ -1,3 +1,6 @@
+import math
+import random
+
 base = '''
 valuable(X) ; not_valuable(X):- gold(X).
 :- #count{X:valuable(X), gold(X)} = FB, #count{X:gold(X)} = B, 10*FB < 6*B.
@@ -8,10 +11,12 @@ valuable(X) ; not_valuable(X):- gold(X).
 wp(P):- PS = #sum{X,Y : gold(Y,X)}, PNS = #sum{X,Y : not_gold(Y,X)}, P = PS + PNS.
 
 #maximize{P : wp(P)}.
+
+#show gold/2.
+#show not_gold/2.
+
 '''
 
-import random
-import math
 
 init_size = 4
 end_size = 30
@@ -21,11 +26,9 @@ for s in range(init_size, end_size + 1):
     for j in range(1, s + 1):
         p = random.random()
         # p = 0.5
-        prob = str((math.log(p))*1000)[:5].replace('.','')
-        n_prob = str((math.log(1 - p))*1000)[:5].replace('.','')
+        prob = str((math.log(p))*1000)[:5].replace('.', '')
+        n_prob = str((math.log(1 - p))*1000)[:5].replace('.', '')
         fp.write("0{gold(" + str(j) + ")}1.\n")
         fp.write(f"gold({j},{prob}):- gold({j}).\n")
         fp.write(f"not_gold({j},{n_prob}):- not gold({j}).\n")
     fp.close()
-
-
